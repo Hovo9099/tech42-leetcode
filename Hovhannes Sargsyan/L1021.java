9a710525bf213baadbd7bf5f6b42d5578ff581e2
Hovo9099 <70805514+Hovo9099@users.noreply.github.com>
2021-04-09 18:09:17 +0400

    Update L1021.java

---
+++ Hovhannes Sargsyan/L1021.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String S) {
+        int first = 0;
+        int end = 0;
+        StringBuilder builder = new StringBuilder();
+        
+        for(int i = 0; i < S.length(); i++) {
+            char a = S.charAt(i);
+            if(a == '(') 
+                first++;
+            else
+                end++;
+            if(first == end) {
+                builder.append(S,i-((first-1)+(end-1)),i);
+                first = 0;
+                end = 0;
+            }
+        }
+        return builder.toString();
+    }
+}
